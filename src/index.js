aba3ab49e3193b951ffa15895da565710d38c6e4
ready-research <72916209+ready-research@users.noreply.github.com>
2021-08-31 20:08:31 +0530

    Security Fix for Prototype Pollution
    Fix prototype pollution when path components are not strings

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export function dset(obj, keys, val) {
 	var i=0, l=keys.length, t=obj, x, k;
 	for (; i < l;) {
 		k = keys[i++];
+		if (typeof k !== 'string' && typeof k !== 'number') { k = String(k); }
 		if (k === '__proto__' || k === 'constructor' || k === 'prototype') break;
 		t = t[k] = (i === l) ? val : (typeof(x=t[k])===typeof(keys)) ? x : (keys[i]*0 !== 0 || !!~(''+keys[i]).indexOf('.')) ? {} : [];
 	}
